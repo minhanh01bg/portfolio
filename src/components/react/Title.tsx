f5d5ec9fb0bfcc3f899e4cced4ea0c0f3ef29113
Christian Deacon <christianmdeacon@gmail.com>
2025-01-31 07:42:47 -0500

    Checkpoint.

---
+++ src/components/react/Title.tsx
@@ -0,0 +1,14 @@
+import { TypeAnimation } from "react-type-animation";
+
+export default function WelcomeTitle() {
+    return (
+        <>
+            <TypeAnimation
+                sequence={["Christian Deacon"]}
+                speed={20}
+                wrapper="h1"
+                className="text-white font-extrabold text-4xl"
+            />
+        </>
+    )
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,38 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            keyframes: {
+                'scale-in': {
+                  '0%': { transform: 'scale(0)' },
+                  '100%': { transform: 'scale(1)' }
+                },
+                'bounce-up': {
+                    '0%': { transform: 'translateY(-60px)' },
+                    '25%': { transform: 'translateY(0px)' },
+                    '50%': { transform: 'translateY(-10px)' },
+                    '100%': { transform: 'translateY(0px)' }
+                  },
+                  'up-down': {
+                    '0%': { transform: 'translateY(-50px)'},
+                    '100%': { transform: 'translateY(0px)'}
+                  }
+              },
+              animation: {
+                'scale-in-slow': 'scale-in 1.0s ease-out',
+                'scale-in-fast': 'scale-in 0.3s ease-out',
+                'bounce-up-slow': 'bounce-up 3.0s ease-out forwards',
+                'bounce-up-fast': 'bounce-up 1.5s ease-out forwards',
+                'up-down-slow': 'up-down 1.0s ease-out',
+                'up-down-fast': 'up-down 0.3s ease-out'
+              },
+              colors: {
+                'primary-1': '#312e81',
+                'primary-2': '#1e1b4b',
+                'secondary-1': '#141234',
+                'secondary-2': '#0c0b20'
+              }
+        },
 	},
 	plugins: [],
 }
