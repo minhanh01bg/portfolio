9aaaebd1c6afd8e922dc9835dcc3b13cb359f73a
Christian Deacon <christianmdeacon@gmail.com>
2025-02-03 13:29:46 -0500

    Decode Base64 email after confirmation.

--- src/components/react/EmailButton.tsx
+++ src/components/react/EmailButton.tsx
@@ -11,13 +11,15 @@ export default function EmailButton({
         <button
             onClick={() => {
                 try {
-                    // We convert from Base64 to attempt to prevent spam bots from getting ahold of the email.
-                    const email = atob(emailHash)
-
                     const c = confirm("Confirm you want to email Christian.")
 
-                    if (c)
+                    if (c) {
+                        // Decode Base64 value to attempt to prevent spam bots from getting ahold of the email so easily.
+                        // Most spam bots don't even utilize JavaScript as well.
+                        const email = atob(emailHash)
+
                         window.location.href = `mailto:${email}`
+                    }
                 } catch (err) {
                     console.error(`Failed to unhash email: ${err}`)
                     alert("Error getting email address.")
