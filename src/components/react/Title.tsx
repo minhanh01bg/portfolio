87a5445a031a7c15b89dc728262cd90979aa4a42
minhanh01bg <vu.anh147852@gmail.com>
2025-08-03 10:23:25 +0700

    Edit main title

--- src/components/react/Title.tsx
+++ src/components/react/Title.tsx
@@ -4,7 +4,7 @@ export default function WelcomeTitle() {
     return (
         <>
             <TypeAnimation
-                sequence={["Christian Deacon"]}
+                sequence={["Minh Anh Vu"]}
                 speed={20}
                 wrapper="h1"
                 className="text-white font-extrabold text-3xl sm:text-4xl font-poppins"
